448ecfe5f4e2d9fb5d84772ce241109c6e59473a
Zab64 <68708785+zabouroboros@users.noreply.github.com>
2022-10-21 22:46:19 -0500

    aprendo-html v0.1
    test

---
+++ Aprendiendo Desarrollo Web Html+Css+Js/js/Codigo.js
@@ -0,0 +1,13 @@
+function h() {
+    alert("HTML es el estandar marca de lenguaje para las paginas web.con html tu puedes crear tu propio web site HTML es facil de aprender lo disfrutaras!");
+  }
+
+
+function c() {
+    alert("Css su traduccion al español hojas estilo de cascadas, siendo un lenguaje de diseño grafico sirve para estilizar y dar nuevas maneras  ");
+  }
+
+  
+function j() {
+    alert("Javascript conocido como js, es un lenguaje de programacion interpretado orientado a objetos permitiendo mejoras en el area del cliente");
+  }
